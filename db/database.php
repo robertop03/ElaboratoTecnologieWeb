871f0444433ea9725311782289a29deacb3f0575
Roberto Pisu <roberto.pisu03@gmail.com>
2025-01-19 11:41:59 +0100

    added query to count notifications unread

--- db/database.php
+++ db/database.php
@@ -748,5 +748,18 @@ class VinoDatabase {
 
         return $this->executeQuery($query, $params);
     }
+
+    // funzione che restituisce il numero di notifiche ancora da leggere di un utente
+    public function getNumeroNotificheNonLette($email){
+        $query = "
+        SELECT COUNT(ID_NOTIFICA) FROM Notifica WHERE Email = :email AND Visualizzato = 'N'
+        ";
+
+        $params = [
+            ':email' => $email
+        ];
+
+        return $this->executeQuery($query, $params);
+    }
 }
 ?>
