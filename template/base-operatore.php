7528078540e94d454209d897efd5e5c615edd7e3
GiacomoRuscelli <giacomo.ruscelli2@studio.unibo.it>
2025-01-20 15:32:29 +0100

    tasto admin dashboard porta alla dashboard

--- template/base-operatore.php
+++ template/base-operatore.php
@@ -14,7 +14,7 @@
     <header class="fixed-top">
       <nav class="navbar navbar-expand-lg" style="background-color: #343a40;">
         <div class="container-fluid">
-          <a class="navbar-brand text-white" href="#">Admin Dashboard</a>
+          <a class="navbar-brand text-white" href="dashboard.php">Admin Dashboard</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
