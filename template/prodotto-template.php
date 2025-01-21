ff61bf70e2c932dc2f6c413a1dfcb8f6c352edf7
Roberto Pisu <roberto.pisu03@gmail.com>
2025-01-21 14:22:29 +0100

    fixed accessibility problem

--- template/prodotto-template.php
+++ template/prodotto-template.php
@@ -69,9 +69,9 @@
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="confirmRemoveLabel">
+            <h3 class="h5 modal-title" id="confirmRemoveLabel">
               <?php echo $linguaAttuale == "en" ? "Remove Favorite" : "Rimuovi Preferito"; ?>
-            </h5>
+            </h3>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
