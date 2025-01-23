792bdc449c64e8d01f09d16219d7b17cbf11f5c7
GiacomoRuscelli <giacomo.ruscelli2@studio.unibo.it>
2025-01-23 12:11:56 +0100

    fix navbar operatore

--- template/base-operatore.php
+++ template/base-operatore.php
@@ -14,7 +14,7 @@
       <nav class="navbar navbar-expand-lg" style="background-color: #343a40;">
         <div class="container-fluid">
           <a class="navbar-brand text-white" href="dashboard.php">Admin Dashboard</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <button class="navbar-toggler bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
           <div class="collapse navbar-collapse" id="navbarNav">
