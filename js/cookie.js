2e9ad9b4a71c02063355209e729fc8c98f390a1f
Roberto Pisu <roberto.pisu03@gmail.com>
2025-01-28 12:07:36 +0100

    add translation cookie

--- js/cookie.js
+++ js/cookie.js
@@ -1,12 +1,9 @@
-
 function acceptCookies() {
-    document.cookie = "cookiesAccepted=true; max-age=86400; path=/"; // Cookie valido per 1 giorno
-    document.getElementById('cookieConsent').style.display = 'none';
+  document.cookie = "cookiesAccepted=true; max-age=86400; path=/" // Cookie valido per 1 giorno
+  document.querySelector("#cookieConsent").style.display = "none"
 }
-window.onload = function() {
-    if (!document.cookie.split('; ').find(row => row.startsWith('cookiesAccepted'))) {
-        document.getElementById('cookieConsent').style.display = 'block';
-    }
+window.onload = function () {
+  if (!document.cookie.split("; ").find((row) => row.startsWith("cookiesAccepted"))) {
+    document.querySelector("#cookieConsent").style.display = "block"
+  }
 }
-
-  
--- template/cookie.php
+++ template/cookie.php
@@ -1,12 +1,12 @@
 <!-- Banner Cookie -->
 <script src="js/cookie.js"></script>
-<div id="cookieConsent" class="alert alert-dark fixed-bottom text-center m-0" role="alert" style="display: none;">
+<div id="cookieConsent" class="alert alert-dark fixed-bottom text-center m-0" role="alert">
     <div class="container">
         <p class="mb-2">
-            Utilizziamo i cookie per migliorare la tua esperienza. Per maggiori informazioni, visita la nostra 
-            <a href="/politica-cookie" class="alert-link text-decoration-underline">Politica sui cookie</a>.
+            <?php echo $linguaAttuale == "en" ? "We use cookies to enhance your experience. For more information, visit our " : "Utilizziamo i cookie per migliorare la tua esperienza. Per maggiori informazioni, visita la nostra " ?>
+            <a href="#" class="alert-link text-decoration-underline"><?php echo $linguaAttuale == "en" ? "Cookie policy" : "Politica sui cookie" ?></a>.
         </p>
-        <button onclick="acceptCookies();" class="btn btn-success btn-sm">Accetto</button>
+        <button onclick="acceptCookies();" class="btn btn-success btn-sm"><?php echo $linguaAttuale == "en" ? "Accept" : "Accetto" ?></button>
     </div>
 </div>
 
